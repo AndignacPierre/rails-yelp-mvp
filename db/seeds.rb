2a0be17007d7be67eac1ca0c22926cf2bfd78849
Pierre Andignac <pierreandignac.work@gmail.com>
2024-08-07 16:06:05 +0200

    restaurant controller done

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,29 @@
 #
 # Example:
 #
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#   ['Action', 'Comedy', 'Drama', 'Horror'].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Cleaning database...'
+sleep 1
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+sleep 1
+
+restaurants = [
+  { name: 'Epicure', address: '75008 Paris', phone_number: '01 42 65 16 77', category: 'french' },
+  { name: 'Sushi Samba', address: '110 Bishopsgate, London EC2N 4AY', phone_number: '020 3640 7330', category: 'japanese' },
+  { name: 'Pizza East', address: '56 Shoreditch High St, London E1 6JJ', phone_number: '020 7729 1888', category: 'italian' },
+  { name: 'Wagamama', address: '1 Bedford Ave, London WC1B 3AU', phone_number: '020 7636 3338', category: 'chinese' },
+  { name: 'Le Pain Quotidien', address: '18 Great Marlborough St, London W1F 7FN', phone_number: '020 7440 7330', category: 'belgian' }
+]
+
+restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+sleep 1
+puts 'Finished!'
